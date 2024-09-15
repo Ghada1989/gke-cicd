46507945ff6abd27ae8cccba2dd60af11547ae25
Ghada1989 <ghada.k.alhader@gmail.com>
2024-09-15 12:38:40 +0300

    changes for today

--- app1/app.py
+++ app1/app.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 # Define a route for the root URL
 @app.route('/')
 def hello_world():
-    return 'Hello, World App 1 My deployment - App Mod 27th of August !'
+    return 'Hello, World App 1 My deployment - App Mod Sep 15th !'
 
 # Run the Flask application if this file is executed directly
 if __name__ == '__main__':
     app.run(debug=True, port=8080,host='0.0.0.0')
+    
